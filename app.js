624cf2b1624b557c5ea3ec88efe01ab6150c7cba
Amir Hossein Heydari <amir123fg5@gmail.com>
2022-05-17 14:10:24 +0430

    add logic and little design

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // DOM, select All element
 
-let containerSquare = document.querySelector(".countainer-ganerate");
+let containerGrid = document.querySelector(".countainer-ganerate");
 let btnBlack = document.querySelector(".btn-black");
 let btnRandom = document.querySelector(".btn-random");
 let btnClear = document.querySelector(".btn-clear");
@@ -8,13 +8,40 @@ let colorPicker = document.querySelector(".color-picker");
 let containerSlider = document.querySelector(".slider-container");
 let slider = document.querySelector(".slider");
 let displayValue = document.querySelector(".display-value");
-let number = 16;
+let number = 16
+function squareGnenerator(slider) {
+    containerGrid.style.gridTemplateColumns = `repeat(${slider}, 1fr)`
+    containerGrid.style.gridTemplateRows = `repeat(${slider} , 1fr)`
 
-function squareGnenerator(containerSquare ,number) {
-    
-    for (let i = 1; i <= number; i++) {
-        
+    for (let i = 1; i <= slider * slider; i++) {
         let square = document.createElement('div')
-        square
+        square.classList.add('grid-element')
+        square.style.border = "1px solid #e8e8e8 "
+        containerGrid.appendChild(square)
     }
+    return containerGrid
 }
+
+
+
+function getSlider (slider) {
+    displayValue.oninput = this.value;
+    displayValue.textContent = `${slider.value} × ${slider.value}`
+}
+
+slider.addEventListener('input' , () => getSlider(slider , displayValue))
+
+
+
+
+function updateSize(value) {
+    containerGrid.innerHTML = ''
+    squareGnenerator(value)
+}
+
+
+
+
+slider.onchange = (e) => updateSize(e.target.value)
+
+window.onload = () => squareGnenerator(number)
