e431e31240d8651a21cb34e83db078942de22610
Amir Hossein Heydari <amir123fg5@gmail.com>
2022-05-17 09:05:50 +0430

    add elements into app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+// DOM, select All element
+
+let containerSquare = document.querySelector(".countainer-ganerate");
+let btnBlack = document.querySelector(".btn-black");
+let btnRandom = document.querySelector(".btn-random");
+let btnClear = document.querySelector(".btn-clear");
+let colorPicker = document.querySelector(".color-picker");
+let containerSlider = document.querySelector(".slider-container");
+let slider = document.querySelector(".slider");
+let displayValue = document.querySelector(".display-value");
+let number = 16;
+
+function squareGnenerator(containerSquare ,number) {
+    
+    for (let i = 1; i <= number; i++) {
+        
+        let square = document.createElement('div')
+        square
+    }
+}
