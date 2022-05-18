cbea81dccb6247d09df3033785b05f760dfcaa84
Amir Hossein Heydari <amir123fg5@gmail.com>
2022-05-18 12:49:24 +0430

    add btn Color Clear etc and clear code for final

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ let colorPicker = document.querySelector(".color-picker");
 let containerSlider = document.querySelector(".slider-container");
 let slider = document.querySelector(".slider");
 let displayValue = document.querySelector(".display-value");
-let squares = document.querySelectorAll('.grid-element')
 
 
 
@@ -19,30 +18,73 @@ slider.onchange = (e) => updateSize(e.target.value)
 window.onload = () => squareGnenerator(number)
 // logic
 
-let number = 16
 
 function squareGnenerator(slider) {
     containerGrid.style.gridTemplateColumns = `repeat(${slider}, 1fr)`
     containerGrid.style.gridTemplateRows = `repeat(${slider} , 1fr)`
 
     for (let i = 1; i <= slider * slider; i++) {
-        let square = document.createElement('div')
-        square.classList.add('grid-element')
-        square.style.border = "1px solid #e8e8e8 "
-        containerGrid.appendChild(square)
+        let gridElement = document.createElement('div')
+        gridElement.classList.add('grid-element')
+
+        // event listener
+        gridElement.addEventListener('mouseover', () => defultColor(gridElement))
+        btnClear.addEventListener('click',() => clear(gridElement))
+        btnRandom.addEventListener('click', () => randomColor(gridElement))
+        btnBlack.addEventListener('click', () => blackColor(gridElement))
+        colorPicker.addEventListener('change', () => fovariteColor(gridElement , colorPicker))
+        // event listener
+        gridElement.style.border = "1px solid #e8e8e8 "
+        containerGrid.appendChild(gridElement)
     }
-    return containerGrid
+} 
+
+// button and color
+
+function defultColor(gridElement) {
+    gridElement.style.backgroundColor = '#000000'
+    gridElement.style.border = "none"
 }
 
+function clear(gridElement) {
+    gridElement.style.backgroundColor = '#fefefe'
+    gridElement.style.border = "1px solid #e8e8e8 "
+}
+
+function randomColor(gridElement) {
+    let randomBetween = (min , max) => 
+    min + Math.floor(Math.random() * (max - min + 1));
+    let r = randomBetween(0, 255)
+    let g = randomBetween(0, 255)
+    let b = randomBetween(0, 255)
+    gridElement.addEventListener('mouseover', () => 
+    gridElement.style.backgroundColor = `rgb(${r}, ${g}, ${b})`) 
+}
+
+function blackColor(gridElement) {
+    gridElement.addEventListener('mouseover', () => defultColor(gridElement))
+}
+
+function fovariteColor(gridElement , colorPicker) {
+    gridElement.addEventListener('mouseover', () => 
+    gridElement.style.backgroundColor = `${colorPicker.value}`) 
+}
+
+// slider
 
 function getSlider (slider) {
     displayValue.oninput = this.value;
     displayValue.textContent = `${slider.value} × ${slider.value}`
 }
 
+
 function updateSize(value) {
-    containerGrid.innerHTML = ''
+    removeContent()
     squareGnenerator(value)
 }
 
-squares.style.backgroundColor = "#1b2d4a"
+
+function removeContent() {
+    containerGrid.innerHTML = ''
+}
+
